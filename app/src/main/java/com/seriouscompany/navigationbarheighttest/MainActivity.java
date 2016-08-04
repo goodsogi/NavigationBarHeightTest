3380847fe436d9d74570813efc7ddfe4fbe81cac
Jeonggyu <goodsogi@gmail.com>
2016-08-04 17:02:19 +0900

    first commit

---
+++ app/src/main/java/com/seriouscompany/navigationbarheighttest/MainActivity.java
@@ -0,0 +1,81 @@
+package com.seriouscompany.navigationbarheighttest;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        showNotificationBarHeight();
+        showNavigationBarHeight();
+    }
+
+    private void showNavigationBarHeight() {
+        TextView navigationBarHeight = (TextView) findViewById(R.id.navigationBarHeight);
+        navigationBarHeight.setText("네비게이션바 높이: " + getNavigationBarHeight());
+    }
+
+    private int getNavigationBarHeight() {
+        Resources resources = getResources();
+        int orientation = Configuration.ORIENTATION_PORTRAIT;
+        int id = resources.getIdentifier(
+                orientation == Configuration.ORIENTATION_PORTRAIT ? "navigation_bar_height" : "navigation_bar_height_landscape",
+                "dimen", "android");
+        if (id > 0) {
+            return resources.getDimensionPixelSize(id);
+        }
+        return 0;
+    }
+
+    private void showNotificationBarHeight() {
+        TextView notificationBarHeight = (TextView) findViewById(R.id.notificationBarHeight);
+        notificationBarHeight.setText("상태바 높이:" +getNotificationBarHeight());
+    }
+
+    private int getNotificationBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
